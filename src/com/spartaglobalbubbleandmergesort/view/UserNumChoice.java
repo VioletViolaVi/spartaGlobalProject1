afe71cbc6e263d006d4e820649045e5af5ed448c
VioletViola <vivian.v18723@gmail.com>
2021-09-29 21:38:48 +0100

    Refactor switch statement.

--- src/com/spartaglobalbubbleandmergesort/view/UserNumChoice.java
+++
@@ -1,4 +0,0 @@
-package com.spartaglobalbubbleandmergesort.view;
-
-public class UserNumChoice {
-}
--- src/com/spartaglobalbubbleandmergesort/view/UsersChoicesInterface.java
+++ src/com/spartaglobalbubbleandmergesort/view/UsersChoicesInterface.java
@@ -10,26 +10,22 @@ public interface UsersChoicesInterface extends AllSortsInterface {
     static void loopedUserAnswers() {
         // allows user to input response
         System.out.println("Choose one: Bubble, Merge or Quick?");
+        System.out.println("Choose a number");
+        int userNumResponse = scanner.nextInt();
 
         // outcomes based on user input
         switch (AllSortsInterface.getUserSortInput()) {
             case "bubble":
-                System.out.println("Choose a number");
-                int userNumResponse = scanner.nextInt();
                 BubbleSort bubbleSort = new BubbleSort();
                 bubbleSort.generateRandArrayOfNums(userNumResponse);
             break;
             case "merge":
-                System.out.println("Choose a number");
-                userNumResponse = scanner.nextInt();
                 MergeSort mergeSort = new MergeSort();
-                mergeSort.generateRandArrayOfNums(userNumResponse); // generateRandArrayOfInts
+                mergeSort.generateRandArrayOfNums(userNumResponse);
             break;
             case "quick":
-                System.out.println("Choose a number");
-                userNumResponse = scanner.nextInt();
                 QuickSort quickSort = new QuickSort();
-                quickSort.generateRandArrayOfNums(userNumResponse); // generateRandomQuickArrayOfInts
+                quickSort.generateRandArrayOfNums(userNumResponse);
             break;
             default:
                 System.out.println("You can only choose between Bubble, Merge or Quick.");
