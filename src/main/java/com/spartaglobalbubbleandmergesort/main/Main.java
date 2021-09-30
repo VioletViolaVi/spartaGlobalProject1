ff7609eb8c33a82b744ecf2368de4638ea378e6d
VioletViola <vivian.v18723@gmail.com>
2021-09-30 23:19:06 +0100

    Add log4j statements.

--- src/main/java/com/spartaglobalbubbleandmergesort/main/Main.java
+++ src/main/java/com/spartaglobalbubbleandmergesort/main/Main.java
@@ -1,9 +1,19 @@
 package com.spartaglobalbubbleandmergesort.main;
 
 import com.spartaglobalbubbleandmergesort.view.UsersChoicesInterface;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Main {
     public static void main(String[] args) {
         UsersChoicesInterface.userAnswers();
+
+        Logger logger = Logger.getLogger("My Application Logger");
+
+        PropertyConfigurator.configure("log4j.properties");
+        logger.warn("This is a message");
+        logger.info("This is a message");
+        logger.error("This is a message");
+        logger.fatal("This is a message");
     }
 }
--- src/test/java/com/spartatestingtestcases/TestMain.java
+++ src/test/java/com/spartatestingtestcases/TestMain.java
@@ -1,15 +1,11 @@
 package com.spartatestingtestcases;
 
-import com.spartaglobalbubbleandmergesort.main.Main;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class TestMain {
-    // for testing:
     private static Logger logger = Logger.getLogger("My Application Logger");
-
-    public static void testingMain() {
-        // for testing:
+    public static void main(String[] args) {
         PropertyConfigurator.configure("log4j.properties");
         logger.warn("This is a message");
     }
